0124606665447dd8eeb872d8fb7f0b2a8d6d950a
jtorres1990 <47399051+jtorres1990@users.noreply.github.com>
2025-04-09 22:46:12 -0500

    conexion a BD

--- src/main/java/com/pokemon/pokeapi/jaxb/PokemonResumenRequest.java
+++ src/main/java/com/pokemon/pokeapi/jaxb/PokemonResumenRequest.java
@@ -3,6 +3,7 @@ package com.pokemon.pokeapi.jaxb;
 
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.Data;
 
@@ -10,10 +11,13 @@ import lombok.Data;
 @XmlAccessorType(XmlAccessType.FIELD)
 @Data
 public class PokemonResumenRequest {
-
+    @XmlElement(namespace = "http://example.com/pokemonservice")
     private Long id;
+    @XmlElement(namespace = "http://example.com/pokemonservice")
     private String pokemonName;
+    @XmlElement(namespace = "http://example.com/pokemonservice")
     private String abilityName;
+    @XmlElement(namespace = "http://example.com/pokemonservice")
     private String typeName;
 
 }
